536ce85a4ed9da70e732236fb6add35cb5288305
AL <roboriotDev4@gmail.com>
2024-02-05 16:58:02 -0800

    Tested more to make speeds more accurate, made smartdashboard update correctly with rpm and shoot  readiness, increased code readability - ARS

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,30 +10,29 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Setup;
 
 public class Shooter extends Subsystem {
+    // naming all physical pieces
     CANSparkMax leftShooterMotor;
     CANSparkMax rightShooterMotor;
     RelativeEncoder leftShooterEncoder;
     RelativeEncoder rightShooterEncoder;
     public Solenoid Solenoid;
+    //naming mutible variables (assigned later)
     public boolean pushSolenoid;
-    public double leftTrig;
-    public boolean leftPush;
-    public boolean leftShoot;
-    public double rightTrig;
-    public boolean rightPush;
-    public boolean rightShoot;
-    public boolean succ;
-    public boolean unSuccFast;
-    public boolean unSuccSlow;
+    public double leftTrig, rightTrig; // how much is each trigger held down
+    public boolean leftPush, rightPush, leftShoot, rightShoot; //is a bumper pressed?, is a trigger pressed enough?
+    public boolean succ, unSuccFast, unSuccSlow; // intake, speaker shoot , amp shoot
+    public boolean timeToShoot;
     public double ShooterFlywheelSpeed;
     public double leftSpeed = -ShooterFlywheelSpeed;
     public double rightSpeed = ShooterFlywheelSpeed;
-    public double speakerSpeed = -100; //all 3 values are pretty much placeholders
-    public double ampSpeed = -2;  // must be slower than speaker
-    public double intakeSpeed = 1.5; // must be opposite of speaker speed
-    public boolean timeToShoot;
+    //initializing constants
+    public double speakerSpeedInput = -0.6; //all 3 values are pretty much placeholders
+    public double speakerSpeedOutput = -3300;
+    public double ampSpeedInput = -0.2;  // must be slower than speaker
+    public double ampSpeedOutput = -1100;
+    public double intakeSpeed = 0.07; // must be opposite of speaker speed
     
-    //get instance
+    // get instance
     static Shooter mInstance = new Shooter();
 
     public static Shooter getInstance() {
@@ -41,39 +40,52 @@ public class Shooter extends Subsystem {
     }
 
     public Shooter(){
+        //initializes all physical pieces
         leftShooterMotor = new CANSparkMax(Setup.ClimberMotorLeftID, MotorType.kBrushless);
         leftShooterEncoder = leftShooterMotor.getEncoder();
         rightShooterMotor = new CANSparkMax(Setup.ClimberMotorRightID, MotorType.kBrushless);
         rightShooterEncoder = rightShooterMotor.getEncoder();
         Solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
     }
+
+    //tests if a flywheel is up to speed and the system is ready to shoot
     public boolean TimetoSpeakerShoot(){
-        return rightShooterEncoder.getVelocity() == speakerSpeed;
+        return rightShooterEncoder.getVelocity() <= speakerSpeedOutput;
     }
+    //sets motor to correct speed
     public void SpeakerShoot() {
         if (unSuccFast){
-            ShooterFlywheelSpeed = speakerSpeed;  
-            
-        }
+            ShooterFlywheelSpeed = speakerSpeedInput;   
+        } 
+        timeToShoot = TimetoSpeakerShoot();
     }
+
+    //tests if a flywheel is up to speed and the system is ready to shoot
     public boolean TimetoAmpShoot(){
-        return leftShooterEncoder.getVelocity() == ampSpeed;
+        return rightShooterEncoder.getVelocity() <= ampSpeedOutput;
     }
+    //sets motor to correct speed
     public void AmpShoot() {
         if (unSuccSlow){
-            ShooterFlywheelSpeed = ampSpeed;
+            ShooterFlywheelSpeed = ampSpeedInput;
         }
+        timeToShoot = TimetoAmpShoot();
   
     }
-  
+
+
     public void Intake() {
         if (succ){
+            //sets motor to correct speed
             ShooterFlywheelSpeed = intakeSpeed; 
         }
     }
 
+
     @Override
     public void updateSubsystem(){
+        //Assigning functions to buttons on secondary contoller 
+        // left and right shoot show if the trigger is pressed down enough
         leftPush = Setup.getInstance().getSecondaryAmpShoot(); 
         leftTrig = Setup.getInstance().getSecondaryAmp();
         leftShoot = leftTrig >0.7;
@@ -81,6 +93,8 @@ public class Shooter extends Subsystem {
         rightTrig = Setup.getInstance().getSecondarySpeaker();
         rightShoot = rightTrig >0.7;
         succ = Setup.getInstance().getSecondaryTelescope()<-0.4;//along with telescoping?
+        
+        //if the correct button is pressed or released, conditionals will be changed for next section
         if(leftShoot){
           unSuccFast = true;
         }else{
@@ -92,7 +106,7 @@ public class Shooter extends Subsystem {
           unSuccSlow = false;
         }
         
-        //Decide flywheel speeds
+        //Decide flywheel speeds and/or direction
         if (succ){
             Intake();
         }else if (unSuccFast){
@@ -119,6 +133,7 @@ public class Shooter extends Subsystem {
     public void outputToSmartDashboard(){
        
         SmartDashboard.putBoolean("time to shoot?", timeToShoot);
+        SmartDashboard.putNumber("RPM", rightShooterEncoder.getVelocity());
 
     }
 
